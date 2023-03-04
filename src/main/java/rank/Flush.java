6bcc58f726ca2c15c16e0948f2a0a3ab7edc5b1e
Claire Tsoi <clairetsoi1129@gmail.com>
2023-03-04 16:26:30 +0000

    Fix - fix the reason message

--- src/main/java/rank/Flush.java
+++ src/main/java/rank/Flush.java
@@ -3,6 +3,7 @@ package rank;
 import core.RankType;
 import core.Value;
 import util.FlushMessageFormatter;
+import util.MessageFormatter;
 
 import java.util.List;
 
@@ -10,6 +11,20 @@ public class Flush extends HighCard{
     public Flush(List<Value> valuesToCompare) {
         super(valuesToCompare);
         rankType = RankType.Flush;
+    }
+
+    @Override
+    public int compareTo(Rank other) {
+        int result = this.getRankType().compareTo(other.getRankType());
+        if (result == 0) { // if both are same rankType, need to compare by higher card
+            result = compareTo(this.getRankType(), this.getValuesToCompare(),
+                    other.getValuesToCompare());
+        } else{ // else compare by rankType
+            reason = new MessageFormatter(result,
+                    this.getRankType(), this.getValuesToCompare(),
+                    other.getValuesToCompare(), 0).format();
+        }
+        return result;
     }
 
     @Override
--- src/main/java/rank/Pair.java
+++ src/main/java/rank/Pair.java
@@ -2,6 +2,7 @@ package rank;
 
 import core.RankType;
 import core.Value;
+import util.MessageFormatter;
 import util.PairMessageFormatter;
 
 import java.util.List;
@@ -10,6 +11,20 @@ public class Pair extends HighCard{
     public Pair(List<Value> valuesToCompare) {
         super(valuesToCompare);
         rankType = RankType.Pair;
+    }
+
+    @Override
+    public int compareTo(Rank other) {
+        int result = this.getRankType().compareTo(other.getRankType());
+        if (result == 0) { // if both are same rankType, need to compare by higher card
+            result = compareTo(this.getRankType(), this.getValuesToCompare(),
+                    other.getValuesToCompare());
+        } else{ // else compare by rankType
+            reason = new MessageFormatter(result,
+                    this.getRankType(), this.getValuesToCompare(),
+                    other.getValuesToCompare(), 0).format();
+        }
+        return result;
     }
 
     @Override
