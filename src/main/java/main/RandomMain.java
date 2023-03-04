e2ca4ad9e64f89bc10d37758b8610d827371663d
Claire Tsoi <clairetsoi1129@gmail.com>
2023-03-04 22:25:11 +0000

    Refactor - remove additional println

--- src/main/java/main/RandomMain.java
+++ src/main/java/main/RandomMain.java
@@ -8,7 +8,6 @@ public class RandomMain {
     public static void main(String[] args){
         RandomCard random = new RandomCardImpl();
         Hand handBlack = new Hand(random.generateCardAvoidConflict(5));
-        System.out.println("handBlack:"+handBlack);
         Hand handWhite = new Hand(random.generateCardAvoidConflict(5));
         System.out.println("handBlack:"+handBlack);
         System.out.println("handWhite:"+handWhite);
