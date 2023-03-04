9b3130375b9ab0efa5000aaea2422c4cbe2800ef
Claire Tsoi <clairetsoi1129@gmail.com>
2023-03-04 09:00:57 +0000

    Add - main class

---
+++ src/main/java/main/Main.java
@@ -0,0 +1,20 @@
+package main;
+
+import model.Hand;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please input 5 cards on black side: eg. JH JC TS QD KH");
+        String blackSide = scanner.nextLine().trim();
+        System.out.println("Please input 5 cards on white side: eg. QC KD 8H AS 8C");
+        String whiteSide = scanner.nextLine().trim();
+
+        Hand handBlack = new Hand(blackSide);
+        Hand handWhite = new Hand(whiteSide);
+        handBlack.compareTo(handWhite);
+        System.out.println(handBlack.getHighCard().getReason());
+    }
+}
